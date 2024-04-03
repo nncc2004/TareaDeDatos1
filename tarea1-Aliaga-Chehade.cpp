3bc615a6c279c1e496b7974538f2655d955d7e04
Nicolas Chehade <nncc2004@gmail.com>
2024-04-03 20:54:15 -0300

    flujo.txt, nombres.txt y se añadieron structs y funciones al .cpp

--- tarea1-Aliaga-Chehade.cpp
+++ tarea1-Aliaga-Chehade.cpp
@@ -1,5 +1,19 @@
 #include<iostream>
 using namespace std;
+struct Pieza {
+ char simbolo; // Define qué tipo de pieza es y su caracter
+ int x, y; // Su posición dentro del tablero [0, 7] x [0, 7]
+};
+
+struct Tablero {
+ Pieza* piezas_tablero; // Lista de piezas que tiene el tablero
+};
+
+bool tableroEnJaqueMate(Tablero &tablero){
+	
+	return true //Por ahora, eso cambiarlo luego
+}
+
 
 int main(){
 	

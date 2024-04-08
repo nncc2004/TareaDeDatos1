e21cc2c27670e4efe772861b9913162933df8dec
Nicolas laptop nuevo <nncc2004@gmail.com>
2024-04-08 09:07:07 -0400

    modularizadas todas las piezas por separado. Falta comentar y revisar indentación

--- tarea1-Aliaga-Chehade.cpp
+++ tarea1-Aliaga-Chehade.cpp
@@ -1,9 +1,8 @@
 #include<iostream>
 #include<fstream>
-
 using namespace std;
 
-
+//Structs predefinidos
 struct Pieza {
 	char simbolo; // Define qué tipo de pieza es y su caracter
 	int x, y; // Su posición dentro del tablero [0, 7] x [0, 7]
@@ -14,6 +13,204 @@ struct Tablero {
 	Pieza* piezas_tablero; // Lista de piezas que tiene el tablero
 };
 
+//Funcion de cada pieza:
+void ReyDeTon(char (&matriz)[8][8], int PosX, int PosY){
+	//rey: 8 posibles posiciones
+	//1 arriba
+	if(PosY-1 >= 0 and matriz[PosY-1][PosX] != 'M'){
+		matriz[PosY-1][PosX] = 'o';
+	} 
+	//2 diagonal superior derecha
+	if(PosX+1 <= 7 and PosY-1 >= 0 and matriz[PosY-1][PosX+1] != 'M'){
+		matriz[PosY-1][PosX+1] = 'o';
+	}
+	//3 derecha
+	if(PosX+1 <= 7 and matriz[PosY][PosX+1] != 'M'){
+		matriz[PosY][PosX+1] = 'o';
+	} 
+	//4 diagonal inferior derecha
+	if(PosX+1 <= 7 and PosY+1 <= 7 and matriz[PosY+1][PosX+1] != 'M'){
+		matriz[PosY+1][PosX+1] = 'o';
+	}
+	//5 abajo
+	if(PosY+1 <= 7 and matriz[PosY+1][PosX] != 'M'){
+		matriz[PosY+1][PosX] = 'o';
+	} 
+	//6 diagonal inferior izquiera
+	if(PosX-1 >= 0 and PosY+1 <= 7 and matriz[PosY+1][PosX-1] != 'M'){
+		matriz[PosY+1][PosX-1] = 'o';
+	}
+	//7 izquierda
+	if(PosX-1 <= 7 and matriz[PosY][PosX-1] != 'M'){
+		matriz[PosY][PosX-1] = 'o';
+	} 
+	//8 diagonal superior izquiera
+	if(PosX-1 >= 0 and PosY-1 >= 0 and matriz[PosY-1][PosX-1] != 'M'){
+		matriz[PosY-1][PosX-1] = 'o';
+	}
+	//Además debe marcar su propia posicion en caso de que nada lo moleste ahí
+	if(matriz[PosY][PosX] != 'M'){
+		matriz[PosY][PosX] = 'o';
+	}
+			
+}
+void ReyDeSeba(char (&matriz)[8][8], int PosX, int PosY){
+	//rey: 8 posibles posiciones
+	if(PosY-1 >= 0){
+		matriz[PosY-1][PosX] = 'M';
+	} //1 arriba
+	if(PosX+1 <= 7 and PosY-1 >= 0 ){
+		matriz[PosY-1][PosX+1] = 'M';
+	}//2 diagonal superior derecha
+	if(PosX+1 <= 7 ){
+		matriz[PosY][PosX+1] = 'M';
+	} //3 derecha
+	if(PosX+1 <= 7 and PosY+1 <= 7){
+		matriz[PosY+1][PosX+1] = 'M';
+	}//4 diagonal inferior derecha
+	if(PosY+1 <= 7){
+		matriz[PosY+1][PosX] = 'M';
+	} //5 abajo
+	if(PosX-1 >= 0 and PosY+1 <= 7){
+		matriz[PosY+1][PosX-1] = 'M';
+	}//6 diagonal inferior izquiera
+	if(PosX-1 <= 7){
+		matriz[PosY][PosX-1] = 'M';
+	} //7 izquierda
+	if(PosX-1 >= 0 and PosY-1 >= 0){
+		matriz[PosY-1][PosX-1] = 'M';
+	}//8 diagonal superior izquiera
+}
+void Reina(char (&matriz)[8][8], int PosX, int PosY){
+	//6 sentidos: vertical, horizontal y las cuatro diagonales.
+	//Es la suma de los movimientos de la torre y el alfil.
+	//1. D. Sup. izquierda
+	for(int n = 1; n<=7;n++){
+		if(PosX-n>=0 and PosY-n>=0){
+			matriz[PosY-n][PosX-n] = 'M';
+		}
+	}
+	//2 D. Sup. derecha
+	for(int n = 1; n<=7;n++){
+		if(PosX+n<=7 and PosY-n>=0){
+			matriz[PosY-n][PosX+n] = 'M';
+		}
+	}
+	//3 D. Inf. izquierda
+	for(int n = 1; n<=7;n++){
+		if(PosX-n>=0 and PosY+n<=7){
+			matriz[PosY+n][PosX-n] = 'M';
+		}
+	}
+	//4 D. Inf. derecha
+	for(int n = 1; n<=7;n++){
+		if(PosX+n<=7 and PosY+n<=7){
+			matriz[PosY+n][PosX+n] = 'M';
+		}
+	}
+	//5 Horizontal 
+	for(int n = 0; n<8; n++){
+		if(n != PosX){  //Esto es para evitar marcar el espacio que esta ocupando pq el rey si se puede mover ahi
+			matriz[PosY][n] = 'M';
+		}
+	}
+	//6 Vertical
+	for(int n = 0; n<8; n++){
+		if(n != PosY){  //Esto es para evitar marcar el espacio que esta ocupando pq el rey si se puede mover ahi
+			matriz[n][PosX] = 'M';
+		}
+	}
+}
+void Peon(char (&matriz)[8][8], int PosX, int PosY){
+	//2 posiciones:
+	//1. diagonal superior derecha
+	if(PosX+1 <= 7 and PosY-1 >= 0 ){
+		matriz[PosY-1][PosX+1] = 'M';
+	}
+	//2 diagonal superior izqierda
+	if(PosX-1 >= 0 and PosY-1 >= 0){
+		matriz[PosY-1][PosX-1] = 'M';
+	}
+}
+void Alfil(char (&matriz)[8][8], int PosX, int PosY){
+	//4 sentidos: D. Sup. izquierda, D. Sup. derecha, D. Inf. izquierda, D. Inf. derecha
+	//1. D. Sup. izquierda
+	for(int n = 1; n<=7;n++){
+		if(PosX-n>=0 and PosY-n>=0){
+			matriz[PosY-n][PosX-n] = 'M';
+		}
+	}
+	//2 D. Sup. derecha
+	for(int n = 1; n<=7;n++){
+		if(PosX+n<=7 and PosY-n>=0){
+			matriz[PosY-n][PosX+n] = 'M';
+		}
+	}
+	//3 D. Inf. izquierda
+	for(int n = 1; n<=7;n++){
+		if(PosX-n>=0 and PosY+n<=7){
+			matriz[PosY+n][PosX-n] = 'M';
+		}
+	}
+	//4 D. Inf. derecha
+	for(int n = 1; n<=7;n++){
+		if(PosX+n<=7 and PosY+n<=7){
+			matriz[PosY+n][PosX+n] = 'M';
+		}
+	}
+}
+void Caballo(char (&matriz)[8][8], int PosX, int PosY){
+	//8 posibles posiciones:
+	//1
+	if(PosX+1 <= 7 and PosY-2 >= 0 ){
+		matriz[PosY-2][PosX+1] = 'M';
+	}
+	//2
+	if(PosX+2 <= 7 and PosY-1 >= 0 ){
+		matriz[PosY-1][PosX+2] = 'M';
+	}
+	//3
+	if(PosX+2 <= 7 and PosY+1 <= 7 ){
+		matriz[PosY+1][PosX+2] = 'M';
+	}
+	//4
+	if(PosX+1 <= 7 and PosY+2 <= 7 ){
+		matriz[PosY+2][PosX+1] = 'M';
+	}
+	//5
+	if(PosX-1 >= 0 and PosY+2 <= 7 ){
+		matriz[PosY+2][PosX-1] = 'M';
+	}
+	//6
+	if(PosX-2 >= 0 and PosY+1 <= 7 ){
+		matriz[PosY+1][PosX-2] = 'M';
+	}
+	//7
+	if(PosX-2 >= 0 and PosY-1 >= 0 ){
+		matriz[PosY-1][PosX-2] = 'M';
+	}
+	//8
+	if(PosX-1 >= 0 and PosY-2 >= 0 ){
+		matriz[PosY-2][PosX-1] = 'M';
+	}
+}
+void Torre(char (&matriz)[8][8], int PosX, int PosY){
+	//2 sentidos: Horizontal y vertical
+	//Horizontal (1)
+	for(int n = 0; n<8; n++){
+		if(n != PosX){  //Esto es para evitar marcar el espacio que esta ocupando pq el rey si se puede mover ahi
+			matriz[PosY][n] = 'M';
+		}
+	}
+	//Vertical (2)
+	for(int n = 0; n<8; n++){
+		if(n != PosY){  //Esto es para evitar marcar el espacio que esta ocupando pq el rey si se puede mover ahi
+			matriz[n][PosX] = 'M';
+		}
+	}
+}
+
+//Función principal
 bool tableroEnJaqueMate(Tablero &tablero){
 	bool Flag = true;
 	//Se crea la matriz de 8x8 y se llena con "v" (de vacío). Luego se imprime con forma matricial
@@ -24,6 +221,7 @@ bool tableroEnJaqueMate(Tablero &tablero){
 		}
 	}
 	
+	
 	//Aquí la lógica para marcar las casillas donde las piezas se pueden mover. 
 	for(int i = 0; i<tablero.cantidad_piezas; i++){
 		//[filas] [columnas] || [y][x]
@@ -32,198 +230,24 @@ bool tableroEnJaqueMate(Tablero &tablero){
 		//matriz[PosY][PosX] = tablero.piezas_tablero[i].simbolo;
 		
 		if(tablero.piezas_tablero[i].simbolo == 'X'){ //Rey de Ton
-			//rey: 8 posibles posiciones
-			//1 arriba
-			if(PosY-1 >= 0 and matriz[PosY-1][PosX] != 'M'){
-				matriz[PosY-1][PosX] = 'o';
-			} 
-			//2 diagonal superior derecha
-			if(PosX+1 <= 7 and PosY-1 >= 0 and matriz[PosY-1][PosX+1] != 'M'){
-				matriz[PosY-1][PosX+1] = 'o';
-			}
-			//3 derecha
-			if(PosX+1 <= 7 and matriz[PosY][PosX+1] != 'M'){
-				matriz[PosY][PosX+1] = 'o';
-			} 
-			//4 diagonal inferior derecha
-			if(PosX+1 <= 7 and PosY+1 <= 7 and matriz[PosY+1][PosX+1] != 'M'){
-				matriz[PosY+1][PosX+1] = 'o';
-			}
-			//5 abajo
-			if(PosY+1 <= 7 and matriz[PosY+1][PosX] != 'M'){
-				matriz[PosY+1][PosX] = 'o';
-			} 
-			//6 diagonal inferior izquiera
-			if(PosX-1 >= 0 and PosY+1 <= 7 and matriz[PosY+1][PosX-1] != 'M'){
-				matriz[PosY+1][PosX-1] = 'o';
-			}
-			//7 izquierda
-			if(PosX-1 <= 7 and matriz[PosY][PosX-1] != 'M'){
-				matriz[PosY][PosX-1] = 'o';
-			} 
-			//8 diagonal superior izquiera
-			if(PosX-1 >= 0 and PosY-1 >= 0 and matriz[PosY-1][PosX-1] != 'M'){
-				matriz[PosY-1][PosX-1] = 'o';
-			}
-			//Además debe marcar su propia posicion en caso de que nada lo moleste ahí
-			if(matriz[PosY][PosX] != 'M'){
-				matriz[PosY][PosX] = 'o';
-			}
-			
+			ReyDeTon(matriz, PosX, PosY);
 		}else if(tablero.piezas_tablero[i].simbolo == 'K'){//Rey de Seba
-			//rey: 8 posibles posiciones
-			if(PosY-1 >= 0){
-				matriz[PosY-1][PosX] = 'M';
-			} //1 arriba
-			if(PosX+1 <= 7 and PosY-1 >= 0 ){
-				matriz[PosY-1][PosX+1] = 'M';
-			}//2 diagonal superior derecha
-			if(PosX+1 <= 7 ){
-				matriz[PosY][PosX+1] = 'M';
-			} //3 derecha
-			if(PosX+1 <= 7 and PosY+1 <= 7){
-				matriz[PosY+1][PosX+1] = 'M';
-			}//4 diagonal inferior derecha
-			if(PosY+1 <= 7){
-				matriz[PosY+1][PosX] = 'M';
-			} //5 abajo
-			if(PosX-1 >= 0 and PosY+1 <= 7){
-				matriz[PosY+1][PosX-1] = 'M';
-			}//6 diagonal inferior izquiera
-			if(PosX-1 <= 7){
-				matriz[PosY][PosX-1] = 'M';
-			} //7 izquierda
-			if(PosX-1 >= 0 and PosY-1 >= 0){
-				matriz[PosY-1][PosX-1] = 'M';
-			}//8 diagonal superior izquiera
-			
+			ReyDeSeba(matriz, PosX, PosY);
 		}else if(tablero.piezas_tablero[i].simbolo == 'R'){//Reina de Seba
-			//6 sentidos: vertical, horizontal y las cuatro diagonales.
-			//Es la suma de los movimientos de la torre y el alfil.
-			//1. D. Sup. izquierda
-			for(int n = 1; n<=7;n++){
-				if(PosX-n>=0 and PosY-n>=0){
-					matriz[PosY-n][PosX-n] = 'M';
-				}
-			}
-			//2 D. Sup. derecha
-			for(int n = 1; n<=7;n++){
-				if(PosX+n<=7 and PosY-n>=0){
-					matriz[PosY-n][PosX+n] = 'M';
-				}
-			}
-			//3 D. Inf. izquierda
-			for(int n = 1; n<=7;n++){
-				if(PosX-n>=0 and PosY+n<=7){
-					matriz[PosY+n][PosX-n] = 'M';
-				}
-			}
-			//4 D. Inf. derecha
-			for(int n = 1; n<=7;n++){
-				if(PosX+n<=7 and PosY+n<=7){
-					matriz[PosY+n][PosX+n] = 'M';
-				}
-			}
-			//5 Horizontal 
-			for(int n = 0; n<8; n++){
-				if(n != PosX){  //Esto es para evitar marcar el espacio que esta ocupando pq el rey si se puede mover ahi
-					matriz[PosY][n] = 'M';
-				}
-			}
-			//6 Vertical
-			for(int n = 0; n<8; n++){
-				if(n != PosY){  //Esto es para evitar marcar el espacio que esta ocupando pq el rey si se puede mover ahi
-					matriz[n][PosX] = 'M';
-				}
-			}
-	
+			Reina(matriz, PosX, PosY);
 		}else if(tablero.piezas_tablero[i].simbolo == 'P'){//Peon de Seba
-			//2 posiciones:
-			//1. diagonal superior derecha
-			if(PosX+1 <= 7 and PosY-1 >= 0 ){
-				matriz[PosY-1][PosX+1] = 'M';
-			}
-			//2 diagonal superior izqierda
-			if(PosX-1 >= 0 and PosY-1 >= 0){
-				matriz[PosY-1][PosX-1] = 'M';
-			}
+			Peon(matriz, PosX, PosY);
 		}else if(tablero.piezas_tablero[i].simbolo == 'A'){//Alfil de seba
-			//4 sentidos: D. Sup. izquierda, D. Sup. derecha, D. Inf. izquierda, D. Inf. derecha
-			//1. D. Sup. izquierda
-			for(int n = 1; n<=7;n++){
-				if(PosX-n>=0 and PosY-n>=0){
-					matriz[PosY-n][PosX-n] = 'M';
-				}
-			}
-			//2 D. Sup. derecha
-			for(int n = 1; n<=7;n++){
-				if(PosX+n<=7 and PosY-n>=0){
-					matriz[PosY-n][PosX+n] = 'M';
-				}
-			}
-			//3 D. Inf. izquierda
-			for(int n = 1; n<=7;n++){
-				if(PosX-n>=0 and PosY+n<=7){
-					matriz[PosY+n][PosX-n] = 'M';
-				}
-			}
-			//4 D. Inf. derecha
-			for(int n = 1; n<=7;n++){
-				if(PosX+n<=7 and PosY+n<=7){
-					matriz[PosY+n][PosX+n] = 'M';
-				}
-			}
+			Alfil(matriz, PosX, PosY);
 		}else if(tablero.piezas_tablero[i].simbolo == 'C'){//Caballo de Seba
-			//8 posibles posiciones:
-			//1
-			if(PosX+1 <= 7 and PosY-2 >= 0 ){
-				matriz[PosY-2][PosX+1] = 'M';
-			}
-			//2
-			if(PosX+2 <= 7 and PosY-1 >= 0 ){
-				matriz[PosY-1][PosX+2] = 'M';
-			}
-			//3
-			if(PosX+2 <= 7 and PosY+1 <= 7 ){
-				matriz[PosY+1][PosX+2] = 'M';
-			}
-			//4
-			if(PosX+1 <= 7 and PosY+2 <= 7 ){
-				matriz[PosY+2][PosX+1] = 'M';
-			}
-			//5
-			if(PosX-1 >= 0 and PosY+2 <= 7 ){
-				matriz[PosY+2][PosX-1] = 'M';
-			}
-			//6
-			if(PosX-2 >= 0 and PosY+1 <= 7 ){
-				matriz[PosY+1][PosX-2] = 'M';
-			}
-			//7
-			if(PosX-2 >= 0 and PosY-1 >= 0 ){
-				matriz[PosY-1][PosX-2] = 'M';
-			}
-			//8
-			if(PosX-1 >= 0 and PosY-2 >= 0 ){
-				matriz[PosY-2][PosX-1] = 'M';
-			}
+			Caballo(matriz, PosX, PosY);
 		}else if(tablero.piezas_tablero[i].simbolo == 'T'){//Torre de Seba
-			//2 sentidos: Horizontal y vertical
-			//Horizontal (1)
-			for(int n = 0; n<8; n++){
-				if(n != PosX){  //Esto es para evitar marcar el espacio que esta ocupando pq el rey si se puede mover ahi
-					matriz[PosY][n] = 'M';
-				}
-			}
-			//Vertical (2)
-			for(int n = 0; n<8; n++){
-				if(n != PosY){  //Esto es para evitar marcar el espacio que esta ocupando pq el rey si se puede mover ahi
-					matriz[n][PosX] = 'M';
-				}
-			}
+			Torre(matriz, PosX, PosY);
 		}
 		
-	}	
+	}
+	//Acá se revisa si es jaque o no
+	//Y por ahora tambíen imprimer la matriz completa	
 	cout<<"\n";
 	for(int y = 0; y<8; y++){
 		for(int x = 0; x<8; x++){
@@ -255,7 +279,6 @@ int main(){
 		for(int x = 0; x<8; x++){
 			caracterActual = lineaActual[x];
 			if(caracterActual != '.'){
-				//cout<<caracterActual<<": ("<<x<<","<< y<<")\n";
 				situacion.piezas_tablero[posicionArreglo].simbolo = caracterActual;
 				situacion.piezas_tablero[posicionArreglo].x = x;
 				situacion.piezas_tablero[posicionArreglo].y =y;

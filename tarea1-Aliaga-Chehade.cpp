9a5d30845675877c95b8f39328770ab2d4a6312b
Nicolas laptop nuevo <nncc2004@gmail.com>
2024-04-10 09:54:24 -0400

    Ordenado y comentado, falta revisar indentación

--- tarea1-Aliaga-Chehade.cpp
+++ tarea1-Aliaga-Chehade.cpp
@@ -13,8 +13,27 @@ struct Tablero {
 	Pieza* piezas_tablero; // Lista de piezas que tiene el tablero
 };
 
-//Funcion de cada pieza:
+//Funciones para cada pieza:
 void ReyDeTon(char (&matriz)[8][8], int PosX, int PosY){
+	/*****
+	* void ReyDeTon
+	******
+	* Resumen Función
+	* Se encarga de marcar las 8 posibles posiciones a las que puede moverse el rey de Ton
+	* Las marca sólo si no está marcada como que otra pieza pueda ponerse ahí
+	* Si se cumple la condición, la marca como una 'o'
+	* Además, marca la casilla en la que se encuentra porque es una posición válida para evitar el jaque
+	******
+	* Input:
+	* char (&matriz)[8][8]:La matriz por referencia para marcar las posiciones
+	* int PosX: coordenada X de la pieza
+	* int PosY: coordenada Y de la pieza
+	******
+	* Returns:
+	* No hay retorno pues es un void
+	* Pero al recibir una matriz por referencia la va modificando
+	*****/
+	 
 	//rey: 8 posibles posiciones
 	//1 arriba
 	if(PosY-1 >= 0 and matriz[PosY-1][PosX] != 'M'){
@@ -55,6 +74,22 @@ void ReyDeTon(char (&matriz)[8][8], int PosX, int PosY){
 			
 }
 void ReyDeSeba(char (&matriz)[8][8], int PosX, int PosY){
+	/*****
+	* void ReyDeSeba
+	******
+	* Resumen Función
+	* Se encarga de marcar las 8 posibles posiciones a las que puede moverse el rey de Sebastián
+	* Marcará las casillas que correspondan con una 'M'
+	******
+	* Input:
+	* char (&matriz)[8][8]:La matriz por referencia para marcar las posiciones
+	* int PosX: coordenada X de la pieza
+	* int PosY: coordenada Y de la pieza
+	******
+	* Returns:
+	* No hay retorno pues es un void
+	* Pero al recibir una matriz por referencia la va modificando
+	*****/
 	//rey: 8 posibles posiciones
 	if(PosY-1 >= 0){
 		matriz[PosY-1][PosX] = 'M';
@@ -82,6 +117,22 @@ void ReyDeSeba(char (&matriz)[8][8], int PosX, int PosY){
 	}//8 diagonal superior izquiera
 }
 void Reina(char (&matriz)[8][8], int PosX, int PosY){
+	/*****
+	* void Reina
+	******
+	* Resumen Función
+	* Se encarga de marcar las 6 posibles direcciones en las que puede moverse la reina
+	* Marcará las casillas que correspondan con una 'M'
+	******
+	* Input:
+	* char (&matriz)[8][8]:La matriz por referencia para marcar las posiciones
+	* int PosX: coordenada X de la pieza
+	* int PosY: coordenada Y de la pieza
+	******
+	* Returns:
+	* No hay retorno pues es un void
+	* Pero al recibir una matriz por referencia la va modificando
+	*****/
 	//6 sentidos: vertical, horizontal y las cuatro diagonales.
 	//Es la suma de los movimientos de la torre y el alfil.
 	//1. D. Sup. izquierda
@@ -122,6 +173,22 @@ void Reina(char (&matriz)[8][8], int PosX, int PosY){
 	}
 }
 void Peon(char (&matriz)[8][8], int PosX, int PosY){
+	/*****
+	* void Peon
+	******
+	* Resumen Función
+	* Se encarga de marcar las 2 posibles posiciones a las que pueden moverse los peones
+	* Marcará las casillas que correspondan con una 'M'
+	******
+	* Input:
+	* char (&matriz)[8][8]:La matriz por referencia para marcar las posiciones
+	* int PosX: coordenada X de la pieza
+	* int PosY: coordenada Y de la pieza
+	******
+	* Returns:
+	* No hay retorno pues es un void
+	* Pero al recibir una matriz por referencia la va modificando
+	*****/
 	//2 posiciones:
 	//1. diagonal superior derecha
 	if(PosX+1 <= 7 and PosY-1 >= 0 ){
@@ -133,6 +200,22 @@ void Peon(char (&matriz)[8][8], int PosX, int PosY){
 	}
 }
 void Alfil(char (&matriz)[8][8], int PosX, int PosY){
+	/*****
+	* void Alfil
+	******
+	* Resumen Función
+	* Se encarga de marcar los 4 sentidos a las que pueden moverse los alfiles
+	* Marcará las casillas que correspondan con una 'M'
+	******
+	* Input:
+	* char (&matriz)[8][8]:La matriz por referencia para marcar las posiciones
+	* int PosX: coordenada X de la pieza
+	* int PosY: coordenada Y de la pieza
+	******
+	* Returns:
+	* No hay retorno pues es un void
+	* Pero al recibir una matriz por referencia la va modificando
+	*****/
 	//4 sentidos: D. Sup. izquierda, D. Sup. derecha, D. Inf. izquierda, D. Inf. derecha
 	//1. D. Sup. izquierda
 	for(int n = 1; n<=7;n++){
@@ -160,6 +243,22 @@ void Alfil(char (&matriz)[8][8], int PosX, int PosY){
 	}
 }
 void Caballo(char (&matriz)[8][8], int PosX, int PosY){
+	/*****
+	* void Caballo
+	******
+	* Resumen Función
+	* Se encarga de marcar las 8 posibles posiciones a las que pueden moverse los caballos
+	* Marcará las casillas que correspondan con una 'M'
+	******
+	* Input:
+	* char (&matriz)[8][8]:La matriz por referencia para marcar las posiciones
+	* int PosX: coordenada X de la pieza
+	* int PosY: coordenada Y de la pieza
+	******
+	* Returns:
+	* No hay retorno pues es un void
+	* Pero al recibir una matriz por referencia la va modificando
+	*****/
 	//8 posibles posiciones:
 	//1
 	if(PosX+1 <= 7 and PosY-2 >= 0 ){
@@ -195,7 +294,23 @@ void Caballo(char (&matriz)[8][8], int PosX, int PosY){
 	}
 }
 void Torre(char (&matriz)[8][8], int PosX, int PosY){
-	//2 sentidos: Horizontal y vertical
+	/*****
+	* void Torre
+	******
+	* Resumen Función
+	* Se encarga de marcar las 2 direcciones en las que pueden moverse las torres
+	* Marcará las casillas que correspondan con una 'M'
+	******
+	* Input:
+	* char (&matriz)[8][8]:La matriz por referencia para marcar las posiciones
+	* int PosX: coordenada X de la pieza
+	* int PosY: coordenada Y de la pieza
+	******
+	* Returns:
+	* No hay retorno pues es un void
+	* Pero al recibir una matriz por referencia la va modificando
+	*****/
+	//2 direcciones: Horizontal y vertical
 	//Horizontal (1)
 	for(int n = 0; n<8; n++){
 		if(n != PosX){  //Esto es para evitar marcar el espacio que esta ocupando pq el rey si se puede mover ahi
@@ -212,8 +327,27 @@ void Torre(char (&matriz)[8][8], int PosX, int PosY){
 
 //Función principal
 bool tableroEnJaqueMate(Tablero &tablero){
+	/*****
+	* bool tableroEnJaqueMate
+	******
+	* Resumen Función
+	* Lo primero que hace es crear un arreglo bidimensional nuevo que simulará el tablero y donde se marcarán 
+	* todas las casillas a las que las diferentes piezas puedan moverse. 
+	* Luego recorre el struct tablero que se obtuvo por referencia desde el main para ir revisando pieza por pieza
+	* Para cada pieza, llamará a la función que corresponda y les pasará las coordenas y la matriz bidimensional como parámetros
+	* Luego de hacerlo con todas las piezas, buscará en la matriz si queda alguna posición a la que el Rey de Ton se pueda mover
+	* sin generar un jaque. También es válido quedarse donde está si así evita el Jaque
+	******
+	* Input:
+	* Tablero &tablero
+	* Este es el struc generado en main() con la información de la cantidad de piezas, y las co
+	* .......
+	******
+	* Returns:
+	* Retorna el valor booleano de Flag, que será false si existe alguna posición segura para el Rey, y true si es que no la hay
+	*****/
 	bool Flag = true;
-	//Se crea la matriz de 8x8 y se llena con "v" (de vacío). Luego se imprime con forma matricial
+	//Se crea la matriz de 8x8 y se llena con '-' en todas las csillas.
 	char matriz[8][8]; //[filas] [columnas] || [y][x]
 	for(int j = 0; j<8; j++){
 		for(int i = 0; i<8; i++){
@@ -227,8 +361,8 @@ bool tableroEnJaqueMate(Tablero &tablero){
 		//[filas] [columnas] || [y][x]
 		int PosX = tablero.piezas_tablero[i].x;
 		int PosY = tablero.piezas_tablero[i].y;
-		//matriz[PosY][PosX] = tablero.piezas_tablero[i].simbolo;
 		
+		//Aquí se llama a la función correspondiente por cada pieza
 		if(tablero.piezas_tablero[i].simbolo == 'X'){ //Rey de Ton
 			ReyDeTon(matriz, PosX, PosY);
 		}else if(tablero.piezas_tablero[i].simbolo == 'K'){//Rey de Seba
@@ -248,22 +382,37 @@ bool tableroEnJaqueMate(Tablero &tablero){
 	}
 	//Acá se revisa si es jaque o no
 	//Y por ahora tambíen imprimer la matriz completa	
-	cout<<"\n";
 	for(int y = 0; y<8; y++){
 		for(int x = 0; x<8; x++){
-			cout<<matriz[y][x];
 			if(matriz[y][x] == 'o'){
 				Flag = false;
 			}
 		}
-		cout<<"\n";
 	}
 	return Flag;
-	
 }
 
 
 int main(){
+	/*****
+	* int main()
+	******
+	* Resumen Función
+	* En esta función se abre el archivo y se extrae la información necesaria.
+	* Lo pirmero es crear todas las variables que vayan a ser necesarias, al igual que la apertura del archivo
+	* Luego se extrae la información importante: La cantidad de piezas y las posiciones de cada una.
+	* Esto se almacena el la variable del struct Tablero llamada "situacion"
+	* Luego esta se pasa a la función TableroEnJaqueMate para que ejecute su lógica.
+	* Por último se imprime si hay jaque según el retorno de la función y se cierra el archivo.
+	******
+	* Input:
+	* No tiene inputs pues es la función main()
+	* .......
+	******
+	* Returns:
+	* Tiene el return 0 de costumbre.
+	*/
+	
 	ifstream archivo;
 	string lineaActual;
 	char caracterActual;
@@ -286,6 +435,7 @@ int main(){
 			}
 		}
 	}
+	
 	if(tableroEnJaqueMate(situacion)){
 		cout<<"Si";
 	}else{
